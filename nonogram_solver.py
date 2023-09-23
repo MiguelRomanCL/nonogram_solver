091fda6013491760e84bfb23bf391bcbbfe7e0bc
Miguel <mnroman@uc.cl>
2023-09-22 23:54:45 -0300

    captamos el error de incongruencia de tablero y un posible stuck. Agregamos métodos iterativos de resolucion

--- nonogram_solver.py
+++ nonogram_solver.py
@@ -1,99 +1,28 @@
-from typing import List
+class AlgorithmStuckException(Exception):
+    pass
+
+
+class IncongruentBoard(Exception):
+    pass
 
 
 class NonoGramBoard:
-    def __init__(self, row_hints: List[List[int]], col_hints: List[List[int]]):
+    def __init__(self, row_hints, col_hints):
+        self.row_hints = row_hints
+        self.col_hints = col_hints
         self.number_of_rows = len(row_hints)
         self.number_of_columns = len(col_hints)
         self.board = [
             [None for _ in range(self.number_of_columns)]
             for _ in range(self.number_of_rows)
         ]
-        self.completed_board = [
-            [None for _ in range(self.number_of_columns)]
-            for _ in range(self.number_of_rows)
-        ]
-        self.row_hints = row_hints
-        self.col_hints = col_hints
-
-    def agrupar_linea(self, linea):
-        grupos = []
-        grupo_actual = 0
-        for celda in linea:
-            if celda == 1:  # Si la celda está coloreada
-                grupo_actual += 1
-            elif grupo_actual:  # Si hemos terminado un grupo
-                grupos.append(grupo_actual)
-                grupo_actual = 0
-        if grupo_actual:
-            grupos.append(grupo_actual)
-        return grupos
-
-    def es_valido(self, linea, numeros):
-        return self.agrupar_linea(linea) == numeros
-
-    def es_posible(self, linea, numeros):
-        return sum(self.agrupar_linea(linea)) <= sum(numeros)
-
-    def es_tablero_valido(self):
-        # Verificar cada fila
-        for i in range(self.number_of_rows):
-            if not self.es_valido(self.board[i], self.row_hints[i]):
-                return False
-        # Verificar cada columna
-        for i in range(self.number_of_columns):
-            if not self.es_valido(
-                [self.board[j][i] for j in range(self.number_of_rows)],
-                self.col_hints[i],
-            ):
-                return False
-        return True
-
-    def mostrar_tablero(self):
-        for fila in self.board:
-            print(" ".join(["#" if c == 1 else "." if c == 0 else "?" for c in fila]))
-
-    def backtrack(self, fila=0, columna=0):
-        if fila == self.number_of_rows:
-            return all(
-                self.es_valido(self.board[i], self.row_hints[i])
-                for i in range(self.number_of_rows)
-            ) and all(
-                self.es_valido(
-                    [self.board[j][i] for j in range(self.number_of_rows)],
-                    self.col_hints[i],
-                )
-                for i in range(self.number_of_columns)
-            )
-        for value in (0, 1):
-            self.board[fila][columna] = value
-            if self.es_posible(
-                self.board[fila], self.row_hints[fila]
-            ) and self.es_posible(
-                [self.board[j][columna] for j in range(self.number_of_rows)],
-                self.col_hints[columna],
-            ):
-                if self.backtrack(fila, columna + 1):
-                    return True
-            self.board[fila][columna] = None
-        return False
-
-    def resolver(self):
-        return self.backtrack()
 
     @staticmethod
     def find_common_positions(strings):
-        """Find positions where all strings coincide and their values."""
-        # Assuming all strings are of the same length
         length = len(strings[0])
-
         result = []
-
         for position in range(length):
-            # Get the character at the current position for the first string
             char = strings[0][position]
-
-            # Check if all other strings have the same character at the same position
             if all(s[position] == char for s in strings[1:]):
                 result.append(char)
             else:
@@ -113,12 +42,8 @@ class NonoGramBoard:
             line += "0" * initial_left_space
             line += "1" * list_of_hints[0]
 
-            number_of_hints -= 1
-
-            if number_of_hints > 0:
+            if number_of_hints > 1:
                 line += "0"
-
-            if number_of_hints > 0:
                 list_of_possible_sub_lines = NonoGramBoard.find_possible_fills(
                     length_of_line - len(line), list_of_hints[1:]
                 )
@@ -132,3 +57,122 @@ class NonoGramBoard:
                 list_of_possible_lines.append(line)
 
         return list_of_possible_lines
+
+    # Define the modified find_possible_fills_partial_info function
+    def find_possible_fills_partial_info(uncompleted_line, list_of_hints):
+        list_of_possible_lines = NonoGramBoard.find_possible_fills(
+            len(uncompleted_line), list_of_hints
+        )
+
+        def matches_uncompleted_line(line):
+            return all(
+                cell == int(line[i]) or cell is None
+                for i, cell in enumerate(uncompleted_line)
+            )
+
+        filtered_lines = [
+            line for line in list_of_possible_lines if matches_uncompleted_line(line)
+        ]
+
+        # Check if filtered_lines is empty and raise IncongruentBoard if it is
+        if len(filtered_lines) == 0:
+            raise IncongruentBoard(
+                "The board is incongruent for the given hints and partial information."
+            )
+
+        return filtered_lines
+
+    def fill_rows(self):
+        for indice_filas in range(self.number_of_rows):
+            row = self.board[indice_filas]
+            hint_actual = self.row_hints[indice_filas]
+
+            llenado_linea = [
+                int(char) if char != "b" else None
+                for char in NonoGramBoard.find_common_positions(
+                    NonoGramBoard.find_possible_fills_partial_info(row, hint_actual)
+                )
+            ]
+
+            for index, value in enumerate(llenado_linea):
+                if value is not None:
+                    self.board[indice_filas][index] = value
+
+    def fill_columns(self):
+        for indice_columnas in range(self.number_of_columns):
+            column = [
+                self.board[j][indice_columnas] for j in range(self.number_of_rows)
+            ]
+            hint_actual = self.col_hints[indice_columnas]
+
+            llenado_linea = [
+                int(char) if char != "b" else None
+                for char in NonoGramBoard.find_common_positions(
+                    NonoGramBoard.find_possible_fills_partial_info(column, hint_actual)
+                )
+            ]
+
+            for index, value in enumerate(llenado_linea):
+                if value is not None:
+                    self.board[index][indice_columnas] = value
+
+    def mostrar_tablero(self):
+        for fila in self.board:
+            print(" ".join(["#" if c == 1 else "." if c == 0 else "?" for c in fila]))
+
+    @staticmethod
+    def is_valid_line(line, hints):
+        groups = []
+        group_count = 0
+        for cell in line:
+            if cell == 1:
+                group_count += 1
+            elif group_count:
+                groups.append(group_count)
+                group_count = 0
+        if group_count:
+            groups.append(group_count)
+        return groups == hints
+
+    def solve_board(self, verbose=True):
+        iterations = 0
+        while True:
+            iterations += 1
+            # Create a deep copy of the current board
+            board_copy = [row.copy() for row in self.board]
+
+            # Try to fill rows and columns
+            self.fill_rows()
+            self.fill_columns()
+
+            # Check if the board has changed
+            if board_copy == self.board:
+                # No changes made, raise UnresolvedBoardException
+                raise AlgorithmStuckException(
+                    "The algorithm is stuck and unable to progress further."
+                )
+
+            # Check if the board is completely filled
+            if all(cell is not None for row in self.board for cell in row):
+                # Verify every row and column against hints
+                all_rows_valid = all(
+                    self.is_valid_line(self.board[i], self.row_hints[i])
+                    for i in range(self.number_of_rows)
+                )
+                all_cols_valid = all(
+                    self.is_valid_line(
+                        [self.board[j][i] for j in range(self.number_of_rows)],
+                        self.col_hints[i],
+                    )
+                    for i in range(self.number_of_columns)
+                )
+
+                if all_rows_valid and all_cols_valid:
+                    if verbose:
+                        print(f"Solved the puzzle in {iterations} iterations.")
+                        self.mostrar_tablero()
+                    return
+                else:
+                    raise AlgorithmStuckException(
+                        "The board is filled but does not match the given hints."
+                    )
